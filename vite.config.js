be724804a310dec8e38e8eeef9e160fc3c06fc82
Hyadawild <kudosinichi9290@gmail.com>
2026-01-14 15:46:23 +0700

    throw all shit

---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    base: './',
+    server: {
+        host: true,
+        port: 5173,
+     strictPort: true,
+        allowedHosts: [
+      '.ngrok-free.app' // allow semua subdomain ngrok
+        ]
+  },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+    
+    plugins: [
+        VitePWA({
+            registerType: 'prompt',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+                cleanupOutdatedCaches: true,
+                // Define runtime caching strategies
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ request }) => request.destination === 'image',
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'images',
+                            expiration: {
+                                maxEntries: 100,
+                                maxAgeSeconds: 60 * 24 * 60 * 60, // 60 Days
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) => request.destination === 'audio' || request.destination === 'video',
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'media',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 60 * 24 * 60 * 60, // 60 Days
+                            },
+                            rangeRequests: true, // Support scrubbing
+                        },
+                    },
+                ],
+            },
+            includeAssets: ['instances.json', 'discord.html'],
+            manifest: false, // Use existing public/manifest.json
+        }),
+    ],
+});
